97d4348c112980694091ab5af8a83729eb9737bb
FakeScoutBot <132735283+FakeScoutBot@users.noreply.github.com>
2026-04-26 20:01:53 +0530

    Fix formatting of callback buttons list
    Signed-off-by: FakeScoutBot <132735283+FakeScoutBot@users.noreply.github.com>

--- anony/helpers/_inline.py
+++ anony/helpers/_inline.py
@@ -61,7 +61,7 @@ class Inline:
                 ]
             ]
         else:
-            cbs = ["admins", "auth", "blist", "cplay", "lang", "ping", "play", "queue", "stats", "sudo"]
+            cbs = ["admins", "auth", "blist", "lang", "ping", "play", "queue", "stats", "sudo"]
             buttons = [
                 self.ikb(text=_lang[f"help_{i}"], callback_data=f"help {cb}")
                 for i, cb in enumerate(cbs)
